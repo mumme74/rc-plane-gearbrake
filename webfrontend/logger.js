98409a714ea5fdf17957fb9fa106fda24bd728ba
Fredrik Johansson <fredrik@mumme.se>
2021-08-22 23:33:36 +0200

    Started word on frontend logger functionallity

---
+++ webfrontend/logger.js
@@ -0,0 +1,368 @@
+"use strict";
+
+/*
+typedef struct {
+  uint8_t size: 2;  // size of data
+  uint8_t type: 6;  // log type
+  uint8_t data[4]; // maximum 4 bytes data is possible to log
+} LogItem_t;
+
+typedef struct {
+  uint8_t length; // number of items
+  uint8_t size;   // number of bytes
+  LogItem_t items[log_end];
+} LogBuf_t;
+*/
+/**
+ * @brief LogItem is a single item, such value of accelerometer
+ */
+class LogItem {
+    static Types = {
+        uninitialized: -1,
+
+        // speed as in wheel revs / sec
+        speedOnGround: 0,
+        wheelRPS_0: 1,
+        wheelRPS_1: 2,
+        wheelRPS_2: 3,
+        // brake force
+        wantedBrakeForce: 4,
+        brakeForce0_out: 5,
+        brakeForce1_out: 6,
+        brakeForce2_out: 7,
+        // wheel slip
+        slip0: 8,
+        slip1: 9,
+        slip2: 10,
+        // steering brakes
+        accelSteering: 11,
+        wsSteering: 12,
+        // accelerometer
+        accel: 13,
+        accelX: 14,
+        accelY: 15,
+        accelZ: 16,
+        
+        // must be last, indicates end of log items
+        log_end: 17,  
+        // special         
+        log_coldStart: 0x3F,
+    }
+
+    static FloatTypes = [
+        LogItem.Types.slip0, LogItem.Types.slip1, LogItem.Types.slip2
+    ];
+
+    static Int8Types = [];
+
+    static Int16Types = [
+        LogItem.Types.accelSteering, LogItem.Types.wsSteering
+    ];
+
+    static Int32Types = [
+        LogItem.Types.accel, LogItem.Types.accelX, 
+        LogItem.Types.accelY, LogItem.Types.accelZ,
+    ];
+
+    startPos = -1;
+    endPos = -1;
+    parent = null;
+    type = LogItem.Types.uninitialized;
+    size = -1;
+    value = null;
+
+    constructor(startPos, parent) {
+        this.startPos = startPos;
+        this.parent = parent;
+        let headerByte = parent.parent.byteArray[startPos];
+        this.size = (headerByte & 0x03) +1; // 0 is 1 byte, 3 is 4 bytes
+        this.type = (headerByte & 0xFC) >> 2;
+        this.endPos = this.startPos + 1 + this.size;
+
+        let vlu = 0, pos = 0;
+        while(1 + startPos + pos < this.endPos)
+          vlu |= (this.parent.parent.byteArray[1 + startPos + pos] << (8 * pos++));
+
+        if (LogItem.FloatTypes.indexOf(this.type) > -1) {
+            // convert to float
+            // inspired by http://cstl-csm.semo.edu/xzhang/Class%20Folder/CS280/Workbook_HTML/FLOATING_tut.htm
+            // and https://en.wikipedia.org/wiki/Single-precision_floating-point_format
+            let sign = ((vlu & 0x80000000) >> 31) ? -1 : 1;
+            let exponent = ((vlu & 0x7F800000) >> 23) -127; // 0=127
+            let mantissa = (vlu & 0x007FFFFF) | 0x00800000; // the 1 from the mantissa is always excluded
+            vlu = sign * Math.pow(2, exponent) *  mantissa * Math.pow(2,-23);
+            //console.log(vlu)
+
+        } else if (LogItem.Int8Types.indexOf(this.type) > -1) {
+            if (vlu & 0x80) vlu = (vlu & 0x7F) * -1;
+        } else if (LogItem.Int16Types.indexOf(this.type) > -1) {
+            if (vlu & 0x8000) vlu = (vlu & 0x7FFF) * -1;
+        } else if (LogItem.Int32Types.indexOf(this.type) > -1) {
+            // 32 bits is so big that the value implicitly goes negative on 32th bit
+            //if (vlu & 0x80000000) vlu = (vlu & 0x7FFFFFFF) -1;
+        }
+        this.value = vlu;
+    }
+}
+
+/**
+ * @brief LogEntry is a log entry, all LogItems that gets logged is contained within this
+ */
+class LogEntry {
+  startPos = -1;
+  parent = null;
+  children = []
+  size = -1;
+
+  constructor(startPos, parent) {
+    this.startPos = startPos;
+    this.parent = parent;
+    this.size = this.parent.byteArray[this.startPos+1];
+  }
+
+  itemCnt() {
+      return this.parent.byteArray[this.startPos]; // first byte is number of items
+  }
+
+  getChild(type) {
+    if (!this.children.length)
+        this._generateChildren();
+    return this.children.find(itm=>itm.type === type);
+  }
+
+  _generateChildren() {
+      this.children = [];
+      let bytePos = this.startPos + 2;
+      for(let i = 0; i < this.itemCnt(); ++i) {
+        let itm = new LogItem(bytePos, this);
+        this.children.push(itm);
+        bytePos += itm.size +1; // +1 for a header byte
+      }
+  }
+}
+
+class LogRoot {
+    static _instance = null;
+
+    static instance() {
+        if (!LogRoot._instance)
+            LogRoot._instance = new LogRoot();
+        return LogRoot._instance;
+    }
+
+    byteArray = new Uint8Array(128 * 1024);
+    logEntries = [];
+    coldStarts = [];
+    startPos = -1;
+
+    parseLog(byteArray, startAddr) {
+        this.byteArray = byteArray;
+        this.startPos = startAddr;
+
+        const readLogEntries = (pos, endPos) => {
+            while (pos < endPos) {
+                let entry = new LogEntry(pos, this);
+                if (entry.size < 1) break;
+                this.logEntries.push(entry);
+                if (entry.itemCnt() === 1 && 
+                    entry.getChild(LogItem.Types.log_coldStart))
+                {
+                  this.coldStarts.push(entry);
+                }
+                pos += entry.size;
+            }
+        }
+
+        // read in the entries from startAddrs up to eof
+        readLogEntries(startAddr, byteArray.length);
+
+        // read in the entries from 0 upt to startAddr
+        readLogEntries(0, this.startPos);
+    }
+}
+
+if (testing) {
+    let testCnt = 0;
+    let test = (vlu, expect)=> {
+        if (vlu !== expect) 
+            console.warn(`fail ${vlu} !== ${expect}`);
+        testCnt++;
+    }
+
+    let logRoot = LogRoot.instance();
+    let bArr = new Uint8Array([
+        0,0,
+        // a cold start entry
+        1, 4,
+        (LogItem.Types.log_coldStart << 2) | 0, 0x5A,
+
+        // log entry with 9 items
+        9, 40, 
+        // first logitem uin8_t, 2 bytes long, positive 64
+        (LogItem.Types.speedOnGround << 2) | 0, 0x40,
+        // logitem 2 int16_t, 3bytes long, positive 0x0132 (306)
+        (LogItem.Types.wsSteering << 2) | 1, 0x32, 0x01,
+        // logitem 3 int16_t 3bytes long, negative 0x8154 (-341)
+        (LogItem.Types.accelSteering << 2) | 1, 0x54, 0x81,
+        // logitem 4 int32_t 5bytes long positive 0x01815405 (25252869)
+        (LogItem.Types.accelX << 2) | 3, 0x05, 0x54, 0x15, 0x01,
+        // logitem 5 int32_t 5 bytes long negative 0x8043210F (-4399376)
+        (LogItem.Types.accelY << 2) | 3, 0x0F, 0x21, 0x43, 0x80,
+        // logitem 6 float 5 bytes long positive 0.25 (0x3E800000)
+        (LogItem.Types.slip0 << 2) | 3, 0x00, 0x00, 0x80, 0x3E,
+        // logitem 6 float 5 bytes long negative -2.0 (0xC0000000)
+        (LogItem.Types.slip1 << 2) | 3, 0x00, 0x00, 0x00, 0xC0,
+        // logitem 6 float 5 bytes long positive pi (0x40490FDB)
+        (LogItem.Types.slip2 << 2) | 3, 0xDB, 0x0F, 0x49, 0x40,
+        // logitem 6 float 5 bytes long positive smallest number 2^-126 (0x00800000)
+        (LogItem.Types.slip0 << 2) | 3, 0x00, 0x00, 0x80, 0x00,
+        // trailing stuff at end
+        0,0,
+
+
+        // a cold start entry
+        1, 4,
+        (LogItem.Types.log_coldStart << 2) | 0, 0x5A,
+
+        // log entry with 13 items
+        13, 48, 
+        // first logitem uin8_t, 2 bytes long, positive 64
+        (LogItem.Types.speedOnGround << 2) | 0, 0x40,
+        // logitem 2 int16_t, 3bytes long, positive 0x0132 (306)
+        (LogItem.Types.wsSteering << 2) | 1, 0x32, 0x01,
+        // logitem 3 int16_t 3bytes long, negative 0x8154 (-341)
+        (LogItem.Types.accelSteering << 2) | 1, 0x54, 0x81,
+        // logitem 4 int32_t 5bytes long positive 0x01815405 (25252869)
+        (LogItem.Types.accelX << 2) | 3, 0x05, 0x54, 0x15, 0x01,
+        // logitem 5 int32_t 5 bytes long negative 0x8043210F (-4399376)
+        (LogItem.Types.accelY << 2) | 3, 0x0F, 0x34, 0x43, 0x80,
+        // logitem 6 float 5 bytes long positive 0.25 (0x3E800000)
+        (LogItem.Types.slip0 << 2) | 3, 0x00, 0x00, 0x80, 0x3E,
+        // logitem 6 float 5 bytes long negative -2.0 (0xC0000000)
+        (LogItem.Types.slip1 << 2) | 3, 0x00, 0x00, 0x00, 0xC0,
+        // logitem 6 float 5 bytes long positive pi (0x40430FDB)
+        (LogItem.Types.slip2 << 2) | 3, 0xDB, 0x0F, 0x43, 0x40,
+        // logitem 6 float 5 bytes long positive smallest number 2^-126 (0x00800000)
+        (LogItem.Types.slip0 << 2) | 3, 0x00, 0x00, 0x80, 0x00,
+        // logitem uint8_t 2 bytes long positive 25
+        (LogItem.Types.wantedBrakeForce << 2) | 0, 0x19,
+        // logitem uint8_t 2 bytes long positive 25
+        (LogItem.Types.brakeForce0_out << 2) | 0, 0x19,
+        // logitem uint8_t 2 bytes long positive 16
+        (LogItem.Types.brakeForce1_out << 2) | 0, 0x10,
+        // logitem uint8_t 2 bytes long positive 20
+        (LogItem.Types.brakeForce2_out << 2) | 0, 0x14,
+
+        // log entry with 13 items
+        13, 48, 
+        // first logitem uin8_t, 2 bytes long, positive 64
+        (LogItem.Types.speedOnGround << 2) | 0, 0x40,
+        // logitem 2 int16_t, 3bytes long, positive 0x0132 (306)
+        (LogItem.Types.wsSteering << 2) | 1, 0x32, 0x01,
+        // logitem 3 int16_t 3bytes long, negative 0x8154 (-341)
+        (LogItem.Types.accelSteering << 2) | 1, 0x54, 0x81,
+        // logitem 4 int32_t 5bytes long positive 0x01815405 (25252869)
+        (LogItem.Types.accelX << 2) | 3, 0x05, 0x54, 0x15, 0x01,
+        // logitem 5 int32_t 5 bytes long negative 0x8043210F (-4399376)
+        (LogItem.Types.accelY << 2) | 3, 0x0F, 0x34, 0x43, 0x80,
+        // logitem 6 float 5 bytes long positive 0.25 (0x3E800000)
+        (LogItem.Types.slip0 << 2) | 3, 0x00, 0x00, 0x80, 0x3E,
+        // logitem 6 float 5 bytes long negative -2.0 (0xC0000000)
+        (LogItem.Types.slip1 << 2) | 3, 0x00, 0x00, 0x00, 0xC0,
+        // logitem 6 float 5 bytes long positive pi (0x40430FDB)
+        (LogItem.Types.slip2 << 2) | 3, 0xDB, 0x0F, 0x43, 0x40,
+        // logitem 6 float 5 bytes long positive smallest number 2^-126 (0x00800000)
+        (LogItem.Types.slip0 << 2) | 3, 0x00, 0x00, 0x80, 0x00,
+        // logitem uint8_t 2 bytes long positive 75
+        (LogItem.Types.wantedBrakeForce << 2) | 0, 0x4B,
+        // logitem uint8_t 2 bytes long positive 75
+        (LogItem.Types.brakeForce0_out << 2) | 0, 0x4B,
+        // logitem uint8_t 2 bytes long positive 16
+        (LogItem.Types.brakeForce1_out << 2) | 0, 0x10,
+        // logitem uint8_t 2 bytes long positive 20
+        (LogItem.Types.brakeForce2_out << 2) | 0, 0x14,
+
+        // a cold start entry
+        1, 4,
+        (LogItem.Types.log_coldStart << 2) | 0, 0x5A,
+
+        // log entry with 13 items
+        13, 48, 
+        // first logitem uin8_t, 2 bytes long, positive 64
+        (LogItem.Types.speedOnGround << 2) | 0, 0x40,
+        // logitem 2 int16_t, 3bytes long, positive 0x0132 (306)
+        (LogItem.Types.wsSteering << 2) | 1, 0x32, 0x01,
+        // logitem 3 int16_t 3bytes long, negative 0x8154 (-341)
+        (LogItem.Types.accelSteering << 2) | 1, 0x54, 0x81,
+        // logitem 4 int32_t 5bytes long positive 0x01815405 (25252869)
+        (LogItem.Types.accelX << 2) | 3, 0x05, 0x54, 0x15, 0x01,
+        // logitem 5 int32_t 5 bytes long negative 0x8043210F (-4399376)
+        (LogItem.Types.accelY << 2) | 3, 0x0F, 0x34, 0x43, 0x80,
+        // logitem 6 float 5 bytes long positive 0.25 (0x3E800000)
+        (LogItem.Types.slip0 << 2) | 3, 0x00, 0x00, 0x80, 0x3E,
+        // logitem 6 float 5 bytes long negative -2.0 (0xC0000000)
+        (LogItem.Types.slip1 << 2) | 3, 0x00, 0x00, 0x00, 0xC0,
+        // logitem 6 float 5 bytes long positive pi (0x40430FDB)
+        (LogItem.Types.slip2 << 2) | 3, 0xDB, 0x0F, 0x43, 0x40,
+        // logitem 6 float 5 bytes long positive smallest number 2^-126 (0x00800000)
+        (LogItem.Types.slip0 << 2) | 3, 0x00, 0x00, 0x80, 0x00,
+        // logitem uint8_t 2 bytes long positive 99
+        (LogItem.Types.wantedBrakeForce << 2) | 0, 0x63,
+        // logitem uint8_t 2 bytes long positive 96
+        (LogItem.Types.brakeForce0_out << 2) | 0, 0x60,
+        // logitem uint8_t 2 bytes long positive 16
+        (LogItem.Types.brakeForce1_out << 2) | 0, 0x10,
+        // logitem uint8_t 2 bytes long positive 20
+        (LogItem.Types.brakeForce2_out << 2) | 0, 0x14,
+    ]);
+
+    logRoot.parseLog(bArr, 2);
+    let itm = logRoot.logEntries[0].getChild(LogItem.Types.log_coldStart);
+    test(itm.size, 1);
+    test(itm.type, LogItem.Types.log_coldStart);
+
+    test(logRoot.logEntries[1].itemCnt(), 9);
+
+    test(logRoot.logEntries.length, 2)
+
+    itm = logRoot.logEntries[1].getChild(LogItem.Types.speedOnGround);
+    test(itm.value, 64);
+    test(itm.type, LogItem.Types.speedOnGround);
+    test(itm.size, 1);
+    test(itm.endPos, 6+4)
+
+    itm = logRoot.logEntries[1].getChild(LogItem.Types.wsSteering);
+    test(itm.value, 306);
+
+    itm = logRoot.logEntries[1].getChild(LogItem.Types.accelSteering);
+    test(itm.value, -340);
+
+    itm = logRoot.logEntries[1].getChild(LogItem.Types.accelX);
+    test(itm.value, 18174981);
+    
+    itm = logRoot.logEntries[1].getChild(LogItem.Types.accelY);
+    test(itm.value, -2143084273);
+
+    itm = logRoot.logEntries[1].getChild(LogItem.Types.slip0);
+    test(itm.value, 0.25);
+
+    itm = logRoot.logEntries[1].getChild(LogItem.Types.slip1);
+    test(itm.value, -2.0);
+
+    itm = logRoot.logEntries[1].getChild(LogItem.Types.slip2);
+    test(itm.value.toPrecision(6), Math.PI.toPrecision(6));
+
+    itm = logRoot.logEntries[1].children[8];
+    test(itm.value, Math.pow(2, -126));
+
+    logRoot.parseLog(bArr, 48);
+    test(logRoot.logEntries.length, 7);
+
+    test(logRoot.logEntries[2].itemCnt(), 1);
+    test(logRoot.logEntries[3].itemCnt(), 13);
+    test(logRoot.logEntries[4].itemCnt(), 13);
+    test(logRoot.logEntries[5].itemCnt(), 1);
+    test(logRoot.logEntries[6].itemCnt(), 13);
+
+    console.log(`have runned ${testCnt} tests`)
+}
--- webfrontend/viewlogpage.js
+++ webfrontend/viewlogpage.js
@@ -1,41 +1,233 @@
 'use strict';
 
-const viewlogHtmlObj = {
-    lang: {
-        en: {
-            header: "View log",
-            p1: `HTML frontend must be loaded by chrome version 89 or later or the latest Edge browser. 
-                 This is due to the Use of Webserial to "talk" to the microcontroller via virtual com port and USB`,
-            fetchLogBtn: "Fetch log from device",
-            fetchedLogPoints: "Fetched log:"
-        },
-        sv: {
-            header: "Visa log",
-            p1: `HTML framände måste ladddas med chrome version 89 eller senare, eller senaste Edge webbläsaren. 
-                 Detta beror på att WebSerial interfacet för att "prata" med mikrocontrollern via virtuel COM port och USB.`,
-            fetchLogBtn: "Hämta log från device",
-            fetchedLogPoints: "Hämtad log:"
-        },
-    },
-    html: (lang) => {
-        return `
-        <div class="w3-row-padding w3-padding-64 w3-container">
-        <div class="w3-content">
-          <div class="w3-twothird">
-            <h1>${viewlogHtmlObj.lang[lang].header}</h1>
+const LogItemTypesTranslated = {
+  uninitialized: {
+    txt: {en: "Unintialized", sv: "Oinitialiserad"},
+    title: {en: "Not valid", sv: "Ej gilltig"}
+  },
 
-            <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top" onclick="location.hash='conf'">
-              ${viewlogHtmlObj.lang[lang].fetchLogBtn}
-            </button>
-            <h5 class="w3-padding-32">${viewlogHtmlObj.lang[lang].fetchedLogPoints}</h5>
-  
-            <p class="w3-text-grey">${viewlogHtmlObj.lang[lang].p1}</p>
-          </div>
-  
-          <div class="w3-third w3-center">
-            <i class="fa fa-anchor w3-padding-64 w3-text-red"></i>
-          </div>
-        </div>
-      </div>`
+  // speed as in wheel revs / sec
+  speedOnGround: {
+    txt: {en: "Wheel revs on ground", sv: "Hjul rotation på marken"},
+    title: {en: "Calculated speed on the ground", sv: "Beräknad hastighet på marken"}
+  },
+  wheelRPS_0: {
+    txt: {en: "Wheel sensor 0", sv: "Hjulsensor 0"},
+    title: {en: "Measured value", sv: "Uppmätt värde"}
+  },
+  wheelRPS_1: {
+    txt: {en: "Wheel sensor 1", sv: "Hjulsensor 1"},
+    title: {en: "Measured value", sv: "Uppmätt värde"}
+  },
+  wheelRPS_2: {
+    txt: {en: "Wheel sensor 2", sv: "Hjulsensor 2"},
+    title: {en: "Measured value", sv: "Uppmätt värde"}
+  },
+  // brake force
+  wantedBrakeForce: {
+    txt: {en: "Requested brakeforce", sv: "Begärd bromskraft"},
+    title: {en: "Brakeforce from reciever", sv: "Bromskraft från mottagaren"}
+  },
+  brakeForce0_out: {
+    txt: {en: "Brake 0 output force", sv: "Broms 0 utkraft"},
+    title: {
+      en: "Brake 0 force sent to wheel brake 0-100%",
+      sv: "Broms 0 kraft sänt till hjulbroms"
     }
+  },
+  brakeForce1_out: {
+    txt: {en: "Brake 1 output force", sv: "Broms 1 utkraft"},
+    title: {
+      en: "Brake 1 force sent to wheel brake 0-100%",
+      sv: "Broms 1 kraft sänt till hjulbroms"
+    }
+  },
+  brakeForce2_out: {
+    txt: {en: "Brake 2 output force", sv: "Broms 2 utkraft"},
+    title: {
+      en: "Brake 2 force sent to wheel brake 0-100%",
+      sv: "Broms 2 kraft sänt till hjulbroms"
+    }
+  },
+  // wheel slip
+  slip0: {
+    txt: {en: "Brake 0 wheel slip", sv: "Broms 0 hjulsläpp"},
+    title: {
+      en: "Brake 0 calculated wheel slippage", 
+      sv: "Broms 0 beräknat hjulsläpp"
+    }
+  },
+  slip1: {
+    txt: {en: "Brake 1 wheel slip", sv: "Broms 1 hjulsläpp"},
+    title: {
+      en: "Brake 1 calculated wheel slippage", 
+      sv: "Broms 1 beräknat hjulsläpp"
+    }
+  },
+  slip2: {
+    txt: {en: "Brake 2 wheel slip", sv: "Broms 2 hjulsläpp"},
+    title: {
+      en: "Brake 2 calculated wheel slippage", 
+      sv: "Broms 2 beräknad hjulsläpp"
+    }
+  },
+  // steering brakes
+  accelSteering: {
+    txt: {en: "Accelerometer steering",  sv: "Accelerometer styrning"},
+    title: {
+      en: "How much steeringbrake due to accelerometer sensing",
+      sv: "Hur mycket styrbroms från accelerometern"
+    }
+  },
+  wsSteering: {
+    txt: {en: "Wheel brake steering", sv: "Hjulbroms styrning"},
+    title: {
+      en: "Wheel brake differential steering based of different sheel speed", 
+      sv: "Hjulbroms styrning baserat på olika hjulhatighet"
+    }
+  },
+  // accelerometer
+  accel: {
+    txt: {en: "Accel. control axis", sv: "Accel. kontroll axel"},
+    title: {
+      en: "Accelerometer control axis value\nThe value form the axis used to steer brake",
+      sv: "Accelerometer kontrol axel värde\nDet värde som används för att styrbromsa"
+    }
+  },
+  accelX: {
+    txt: {en: "Accelerometer X", sv: "Accelerometer X"},
+    title: {
+      en: "Accelerometer value for X-axis",
+      sv: "Accelerometer värde för X-axeln"
+    }
+  },
+  accelY: {
+    txt: {en: "Accelerometer Y", sv: "Accelerometer Y"},
+    title: {
+      en: "Accelerometer value for Y-axis",
+      sv: "Accelerometer värde för Y-axeln"
+    }
+  },
+  accelZ: {
+    txt: {en: "Accelerometer Y", sv: "Accelerometer Y"},
+    title: {
+      en: "Accelerometer value for Y-axis",
+      sv: "Accelerometer värde för Y-axeln"
+    }
+  },
+  
+  // must be last, indicates end of log items
+  log_end: {txt: {en: "Log end", sv: "Log slut"}},  
+  // special         
+  log_coldStart: {
+    txt: {en: "Start from reset", sv: "Uppstart från reset"},
+    title: {
+      en: "A special log pint to mark a device restart.\nUse to find out when a new flying session started",
+      sv: "En speciell log punkt för att markera en omstart.\nAnvänd för att se när en ny flygsession börjar"
+    },
+  },
+}
+
+const viewlogHtmlObj = {
+  fetchLog: ()=>{
+    console.log("Fetch log from device")
+  },
+  clearLog: ()=>{
+    console.log("Clear log in device")
+  },
+  selectStart: (evt, idx) => {
+    console.log("view log " + idx);
+    const lang = document.querySelector("html").lang;
+    const latest = viewlogHtmlObj.lang[lang].latestSession;
+    const j = LogRoot.instance().coldStarts.length - 1 - idx;
+    const txt = j === 0 ? latest : latest + " - " + j;
+    evt.target.parentNode.previousElementSibling.innerText = txt;
+
+    // create dropdown to select what items to show
+
+  },
+  lang: {
+    en: {
+      header: "View log",
+      p1: `HTML frontend must be loaded by chrome version 89 or later or the latest Edge browser. 
+            This is due to the Use of Webserial to "talk" to the microcontroller via virtual com port and USB`,
+      fetchLogBtn: "Fetch log from device",
+      clearLogBtn: "Clear log in device",
+      fetchedLogPoints: "Fetched log:",
+      selectLog: "Select log",
+      latestSession: "Latest session",
+      showLogItem: "Show log items",
+      chooseAll: "Choose all",
+    },
+    sv: {
+      header: "Visa logg",
+      p1: `HTML framände måste laddas med chrome version 89 eller senare, eller senaste Edge webbläsaren. 
+            Detta beror på att WebSerial interfacet för att "prata" med mikrocontrollern via virtuel COM port och USB finns inte tillgänglig innan dessa`,
+      fetchLogBtn: "Hämta logg från enhet",
+      clearLogBtn: "Nollställ loggminne i enhet",
+      fetchedLogPoints: "Hämtad logg:",
+      selectLog: "Välj logg",
+      latestSession: "Senaste session",
+      showLogItem: "Visa logg saker",
+      chooseAll: "Välj alla",
+    },
+  },
+  html: (lang) => {
+    const tr = viewlogHtmlObj.lang[lang];
+
+    const logRoot = LogRoot.instance();
+    let starts = [];
+    for (let i = logRoot.coldStarts.length -1, j = 0; i > -1 ; --i, ++j) {
+      const latest = tr.latestSession;
+      const txt = j === 0 ? latest : latest + " - " + j;
+      starts.push(`<button class="w3-bar-item w3-button" onclick="viewlogHtmlObj.selectStart(event, ${i})">${txt}</button>`);
+    }
+
+    return `
+      <div class="w3-row-padding w3-padding-64 w3-container">
+      <div class="w3-content">
+        <div class="">
+          <h1>${tr.header}</h1>
+
+          <button class="w3-button w3-blue w3-padding-large w3-large w3-margin-top" onclick="viewlogHtmlObj.fetchLog()">
+            ${tr.fetchLogBtn}
+          </button> 
+          <button class="w3-button w3-orange w3-padding-large w3-large w3-margin-top" onclick="viewlogHtmlObj.clearLog()">
+            ${tr.clearLogBtn}
+          </button>
+          <h5 class="w3-padding-8">${tr.fetchedLogPoints}</h5>
+          <div class="w3-bar w3-light-grey">
+            <!--<button class="w3-bar-item w3-button">Home</button>
+            <button class="w3-bar-item w3-button">Link 1</button>-->
+            <div class="w3-dropdown-hover">
+              <button class="w3-button">${tr.selectLog}</button>
+              <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                ${starts.join("\n")}
+              </div>
+            </div>
+            <div class="w3-dropdown-hover" id="showLogItm">
+              <button class="w3-button">${tr.showLogItem}</button>
+              <div class="w3-dropdown-content w3-bar-block w3-card-4" id="showLogItemDropdown">
+                <label class="w3-bar-item w3-button"><input type="checkbox" class="w3-button"/> ${tr.chooseAll}</label>
+              </div>
+            </div>
+          </div>
+          
+          <div class="w3-row">
+            <div class="w3-col s4">
+              left
+            </div>
+            <div class="w3-col s8">
+              
+            </div>
+          </div>
+          <p class="w3-text-grey">${viewlogHtmlObj.lang[lang].p1}</p>
+        </div>
+
+        <div class="w3-third w3-center">
+          <i class="fa fa-anchor w3-padding-64 w3-text-red"></i>
+        </div>
+      </div>
+    </div>`
+  }
 };
