0d2b2e31a456615a2c084d191ebb088cfbf0504f
Fredrik Johansson <fredrik@mumme.se>
2021-11-22 07:07:27 +0100

    Whitespace

--- webfrontend/widgets/chartwidget.js
+++ webfrontend/widgets/chartwidget.js
@@ -4,17 +4,17 @@ let ChartWidget;
 {
 const stepFactor = 15,
       headerHeight = 50,
-	  horizBarHeight = 50,
-	  vertBarWidth = 20,
-	  canvasMinWidth = 50,
-	  canvasHeight = 600,
-	  origoAt = {x: vertBarWidth, y: canvasHeight / 2 -1};
+      horizBarHeight = 50,
+      vertBarWidth = 20,
+      canvasMinWidth = 50,
+      canvasHeight = 600,
+      origoAt = {x: vertBarWidth, y: canvasHeight / 2 -1};
 const axisColors = [
-	'#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
-	'#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
-	'#008080', '#e6beff', '#9a6324', '#fffac8', '#800000',
-	'#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080',
-	'#ffffff', '#000000'];
+    '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
+    '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
+    '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000',
+    '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080',
+    '#ffffff', '#000000'];
 
 class ChartWidgetCls extends WidgetBaseCls {
   selectedType = -1;
@@ -23,26 +23,26 @@ class ChartWidgetCls extends WidgetBaseCls {
   constructor(shownColumns, parentNode) {
     super(shownColumns);
 
-	// the header of the chart
-	this.headerNode = document.createElement("div");
-	this.headerNode.style.cssText = `position:sticky;left:0px;text-align:center;padding:5px;border-radius:5px;background-color:#FFF`;
-	this.headerColor = document.createElement("div");
-	this.headerColor.style.cssText = "display:inline-block;min-height: 30px;min-width:60px;background-color:#FFF;margin: 5px;";
-	this.headerNode.appendChild(this.headerColor);
-	this.headerSpan = document.createElement("span");
-	this.headerNode.appendChild(this.headerSpan);
-	parentNode.appendChild(this.headerNode)
+    // the header of the chart
+    this.headerNode = document.createElement("div");
+    this.headerNode.style.cssText = `position:sticky;left:0px;text-align:center;padding:5px;border-radius:5px;background-color:#FFF`;
+    this.headerColor = document.createElement("div");
+    this.headerColor.style.cssText = "display:inline-block;min-height: 30px;min-width:60px;background-color:#FFF;margin: 5px;";
+    this.headerNode.appendChild(this.headerColor);
+    this.headerSpan = document.createElement("span");
+    this.headerNode.appendChild(this.headerSpan);
+    parentNode.appendChild(this.headerNode)
 
-	// create a canvas to draw on
+    // create a canvas to draw on
     this.rootNode = document.createElement("canvas");
     this.rootNode.height = canvasHeight;
     this.rootNode.width = canvasMinWidth;
-	this.contex = this.rootNode.getContext('2d');
+    this.contex = this.rootNode.getContext('2d');
     parentNode.appendChild(this.rootNode);
 
-	// attach mousemove/down
-	this.rootNode.addEventListener("mousemove", this._touchmove.bind(this));
-	this.rootNode.addEventListener("mousedown", this._touchstart.bind(this));
+    // attach mousemove/down
+    this.rootNode.addEventListener("mousemove", this._touchmove.bind(this));
+    this.rootNode.addEventListener("mousedown", this._touchstart.bind(this));
   }
 
   /**
@@ -52,16 +52,16 @@ class ChartWidgetCls extends WidgetBaseCls {
    */
   setData(colData, data) {
     this.rootNode.width =
-	  Math.max(canvasMinWidth, data.length * stepFactor + vertBarWidth);
+      Math.max(canvasMinWidth, data.length * stepFactor + vertBarWidth);
     super.setData(colData, data);
   }
 
   render() {
-	console.time("render")
-	this.clear();
-	super.render();
+    console.time("render")
+    this.clear();
+    super.render();
     this._renderHorizontalBar();
-	this._renderVerticalBar();
+    this._renderVerticalBar();
     if (!this.data.length) return;
 
     // render each axis
@@ -70,18 +70,18 @@ class ChartWidgetCls extends WidgetBaseCls {
         const child = this.data[0].getChild(col.entry.type);
         if (!child) continue;
         this._renderAxel(this.data[0].children.indexOf(child), col);
-	  }
+      }
     }
 
-	console.timeEnd("render")
+    console.timeEnd("render")
   }
 
   clear() {
     super.clear();
-	this.contex.beginPath();
-	this.contex.fillStyle = '#FFF';
+    this.contex.beginPath();
+    this.contex.fillStyle = '#FFF';
     this.contex.fillRect(0, 0, this.rootNode.width, this.rootNode.height);
-	this.contex.stroke();
+    this.contex.stroke();
   }
 
   downloadData() {
@@ -105,16 +105,16 @@ class ChartWidgetCls extends WidgetBaseCls {
     this.contex.moveTo(origoAt.x, origoAt.y - factor * itm.realVlu());
     for(let i = 1; i < this.data.length; ++i) {
       itm = this.data[i].children[axelIdx];
-	  this.contex.lineTo(origoAt.x + i * stepFactor, origoAt.y - factor * itm.realVlu());
+      this.contex.lineTo(origoAt.x + i * stepFactor, origoAt.y - factor * itm.realVlu());
     }
     this.contex.stroke();
   }
 
   _renderHorizontalBar() {
     // render the horizontal bar
-	this.contex.fillStyle = '#FFF';
+    this.contex.fillStyle = '#FFF';
     this.contex.fillRect(0, this.rootNode.height - horizBarHeight,
-		this.rootNode.width, horizBarHeight);
+        this.rootNode.width, horizBarHeight);
 
     this.contex.beginPath();
     this.contex.lineWidth = "5";
@@ -123,14 +123,14 @@ class ChartWidgetCls extends WidgetBaseCls {
     this.contex.lineTo(this.rootNode.width, origoAt.y);
     // do the small lines for each entry
     for(let i = 0; i < this.data.length; ++i) {
-		this.contex.moveTo(vertBarWidth + i * stepFactor, origoAt.y-2);
-		this.contex.lineTo(vertBarWidth + i * stepFactor, origoAt.y+4);
+        this.contex.moveTo(vertBarWidth + i * stepFactor, origoAt.y-2);
+        this.contex.lineTo(vertBarWidth + i * stepFactor, origoAt.y+4);
     }
     this.contex.stroke();
   }
 
   _renderVerticalBar() {
-	this.contex.fillStyle = '#FFF';
+    this.contex.fillStyle = '#FFF';
     this.contex.fillRect(0, 0, vertBarWidth, this.rootNode.height);
     this.contex.beginPath();
     this.contex.lineWidth = "5";
@@ -141,38 +141,38 @@ class ChartWidgetCls extends WidgetBaseCls {
   }
 
   _selectAType(selectType) {
-	const reRender = this.selectedType !== selectType;
-	this.selectedType = selectType;
+    const reRender = this.selectedType !== selectType;
+    this.selectedType = selectType;
 
-	if (reRender) {
+    if (reRender) {
       this.render();
 
-	  if (selectType > -1) {
-	  	const types = Object.keys(LogItem.Types).slice(1);
-	  	console.log("match start", types[selectType]);
+      if (selectType > -1) {
+          const types = Object.keys(LogItem.Types).slice(1);
+          console.log("match start", types[selectType]);
         this.headerSpan.innerText = types[selectType];
-	    this.headerColor.style.backgroundColor = axisColors[selectType];
-	  } else
-	    this.headerColor.style.backgroundColor = "#FFF";
+        this.headerColor.style.backgroundColor = axisColors[selectType];
+      } else
+        this.headerColor.style.backgroundColor = "#FFF";
 
 
 
-	  // hide/show
+      // hide/show
       this.headerNode.style.backgroundColor = selectType < 0 ? "#FFF" : "#CCC";
-	}
+    }
   }
 
   _touchmove(evt) {
     const logItm = this._mapPointToLogItem({x: evt.clientX, y: evt.clientY});
-	if (logItm) {
+    if (logItm) {
       const types = Object.keys(LogItem.Types).slice(1);
       console.log("match move", types[logItm.type]);
-	}
+    }
   }
 
   _touchstart(evt) {
     const logItm = this._mapPointToLogItem({x: evt.clientX, y: evt.clientY});
-	this._selectAType(logItm ? logItm.type : -1);
+    this._selectAType(logItm ? logItm.type : -1);
   }
 
   _mapPointToLogItem(pnt) {
@@ -182,14 +182,14 @@ class ChartWidgetCls extends WidgetBaseCls {
     const x = Math.round((realX - vertBarWidth) / stepFactor),
           y = Math.floor(origoAt.y - realY);
 
-	for(const itm of this.data[x].children.values()) {
+    for(const itm of this.data[x].children.values()) {
       const info = LogItem.Types.info(itm.type);
       const factor = origoAt.y / info.max;
-	  const itmY = Math.round(factor * itm.realVlu());
+      const itmY = Math.round(factor * itm.realVlu());
       if (y <= itmY + 2 && y >= itmY -2) {
-		return itm;
-	  }
-	}
+        return itm;
+      }
+    }
   }
 }
 ChartWidget = ChartWidgetCls;
