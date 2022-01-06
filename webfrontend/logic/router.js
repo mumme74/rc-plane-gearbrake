8cce87900b7c457745b77dc54c642330d8086383
Fredrik Johansson <fredrik@mumme.se>
2022-01-07 00:33:19 +0100

    Whitespace

--- webfrontend/logic/router.js
+++ webfrontend/logic/router.js
@@ -1,32 +1,32 @@
 "use strict";
 
 function routeMainContent() {
-    let lang = document.querySelector("html").lang;
-    let parts = location.hash.replace(/^#/, "").split("&");
-    let page = parts[0];
-    let htmlFunctor;
+  let lang = document.querySelector("html").lang;
+  let parts = location.hash.replace(/^#/, "").split("&");
+  let page = parts[0];
+  let htmlFunctor;
 
-    switch(page) {
-    case 'viewlog':
-        htmlFunctor = viewlogHtmlObj.html.bind(viewlogHtmlObj);
-        break;
-    case 'conf':
-        htmlFunctor = configureHtmlObj.html;
-        break;
-    case 'settings':
-        htmlFunctor = appSettingsHtmlObj.html;
-        break;
-    case 'start': // fallthrough
-    default:
-        htmlFunctor = welcomeHtmlObj.html;
-    }
+  switch(page) {
+  case 'viewlog':
+    htmlFunctor = viewlogHtmlObj.html.bind(viewlogHtmlObj);
+    break;
+  case 'conf':
+    htmlFunctor = configureHtmlObj.html;
+    break;
+  case 'settings':
+    htmlFunctor = appSettingsHtmlObj.html;
+    break;
+  case 'start': // fallthrough
+  default:
+    htmlFunctor = welcomeHtmlObj.html;
+  }
 
-    htmlFunctor(document.getElementById("content"), lang);
+  htmlFunctor(document.getElementById("content"), lang);
 }
 
 document.addEventListener('DOMContentLoaded', routeMainContent);
 window.addEventListener('hashchange', (evt)=>{
-    evt.stopPropagation();
-    evt.preventDefault();
-    routeMainContent();
+  evt.stopPropagation();
+  evt.preventDefault();
+  routeMainContent();
 });
